89ed29325792d606c7eefc18e5290765e8b6ece9
mbilal <mbilal@sirma.com>
2022-01-23 13:39:24 +0500

    added simple home and about page

---
+++ src/pages/About.page.tsx
@@ -0,0 +1,13 @@
+import { Container, Row, Col } from 'react-bootstrap'
+
+const About = () => {
+  return (
+    <Container fluid>
+      <Row>
+        <Col>About Page</Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default About
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,1 +1,2 @@
-export {default as Home} from './Home.page'
+export { default as Home } from './Home.page'
+export { default as About } from './About.page'
