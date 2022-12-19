a6db23ef4e3f15f648a9638e0355189aeec474b8
Bilal <mbilal@sirma.com>
2022-12-19 23:51:28 +0500

    null setLastLocation in cancelNavigation function

--- src/hooks/useCallbackPrompt.ts
+++ src/hooks/useCallbackPrompt.ts
@@ -1,48 +1,54 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useCallback, useEffect, useState } from 'react';
-import { useLocation, useNavigate } from 'react-router';
-import { useBlocker } from './useBlocker';
+import { useCallback, useEffect, useState } from 'react'
+import { useLocation, useNavigate } from 'react-router'
+import { useBlocker } from './useBlocker'
 
 export function useCallbackPrompt(when: boolean): (boolean | (() => void))[] {
-  const navigate = useNavigate();
-  const location = useLocation();
-  const [showPrompt, setShowPrompt] = useState(false);
-  const [lastLocation, setLastLocation] = useState<any>(null);
-  const [confirmedNavigation, setConfirmedNavigation] = useState(false);
+  const navigate = useNavigate()
+  const location = useLocation()
+  const [showPrompt, setShowPrompt] = useState(false)
+  const [lastLocation, setLastLocation] = useState<any>(null)
+  const [confirmedNavigation, setConfirmedNavigation] = useState(false)
 
   const cancelNavigation = useCallback(() => {
-    setShowPrompt(false);
-  }, []);
+    setShowPrompt(false)
+    setLastLocation(null)
+  }, [])
 
   // handle blocking when user click on another route prompt will be shown
   const handleBlockedNavigation = useCallback(
     (nextLocation) => {
-        // in if condition we are checking next location and current location are equals or not
+      // in if condition we are checking next location and current location are equals or not
       if (
         !confirmedNavigation &&
         nextLocation.location.pathname !== location.pathname
       ) {
-        setShowPrompt(true);
-        setLastLocation(nextLocation);
-        return false;
+        setShowPrompt(true)
+        setLastLocation(nextLocation)
+        return false
       }
-      return true;
+      return true
     },
-    [confirmedNavigation],
-  );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [confirmedNavigation, location]
+  )
 
   const confirmNavigation = useCallback(() => {
-    setShowPrompt(false);
-    setConfirmedNavigation(true);
-  }, []);
+    setShowPrompt(false)
+    setConfirmedNavigation(true)
+  }, [])
 
   useEffect(() => {
     if (confirmedNavigation && lastLocation) {
-      navigate(lastLocation.location.pathname);
+      navigate(lastLocation.location?.pathname)
+
+      // Clean-up state on confirmed navigation
+      setConfirmedNavigation(false)
     }
-  }, [confirmedNavigation, lastLocation]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [confirmedNavigation, lastLocation])
 
-  useBlocker(handleBlockedNavigation, when);
+  useBlocker(handleBlockedNavigation, when)
 
-  return [showPrompt, confirmNavigation, cancelNavigation];
+  return [showPrompt, confirmNavigation, cancelNavigation]
 }
